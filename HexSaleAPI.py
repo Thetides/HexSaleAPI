7832d8f99bd96f6bda7e316cb05b67c3f1c64373
TheTides <luke.milby@gmail.com>
2017-11-05 11:57:07 -0600

    added all articles and sets methods

--- HexSaleAPI.py
+++ HexSaleAPI.py
@@ -7,7 +7,7 @@ def _url(path):
     return 'https://api.hexsales.net/v1' + path
 
 def get_articles():
-    return requests.get(_url('/articles/'))
+    return requests.get(_url('/articles'))
 
 def get_summaries():
     return requests.get(_url('/summaries'))
@@ -24,5 +24,16 @@ def get_mostsold():
 def post_search(article):
     return requests.post(_url('/articles/search'), json={"name":article})
 
+def get_articles_uuid(uuid):
+    return requests.get(_url('/articles/{}'.format(uuid)))
 
-print(post_search('Runebind').content)
+def get_articles_histories(uuid):
+    return requests.get(_url('/articles/{}/histories'.format(uuid)))
+
+def get_articles_summaries(uuid):
+    return requests.get(_url('/articles/{}/summaries'.format(uuid)))
+
+def get_sets():
+    return requests.get(_url('/sets'))
+
+#print(post_search('Runebind').content)
